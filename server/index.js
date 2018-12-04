864a72ffd65e6f74e96102b490663de6599dc205
Steven Lee <stevenyslee@gmail.com>
2018-12-04 14:29:56 -0800

    Serve HTML

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '/../dist'));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
